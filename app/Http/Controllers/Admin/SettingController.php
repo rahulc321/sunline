868eeb600d7b1abbf5ffab2e21d23a83b4dc543d
Rahul <rahulidcsoftwares@gmail.com>
2025-11-12 08:48:21 +0530

    ip changes

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -13,6 +13,7 @@ class SettingController extends Controller
         $settings = [
             'heatpump_commission' => Setting::getValue('heatpump_commission', 0),
             'aircon_commission'   => Setting::getValue('aircon_commission', 0),
+            'office_ip_address'   => Setting::getValue('office_ip_address', 0),
         ];
 
         return view('admin.settings.index', compact('settings'));
@@ -23,10 +24,12 @@ class SettingController extends Controller
         $request->validate([
             'heatpump_commission' => 'required|numeric|min:0',
             'aircon_commission'   => 'required|numeric|min:0',
+            'office_ip_address'   => 'nullable',
         ]);
 
         Setting::setValue('heatpump_commission', $request->heatpump_commission);
         Setting::setValue('aircon_commission', $request->aircon_commission);
+        Setting::setValue('office_ip_address', $request->office_ip_address);
 
         return back()->with('success', 'Settings updated successfully!');
     }
